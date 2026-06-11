77296ceaaf9d96623c17ced7938d4e403566111c
AliHamzaRiaz <hamzagujjarriaz3@gmail.com>
2026-06-11 09:59:29 -0700

    Fix Vercel API endpoints using native serverless functions

---
+++ api/products.js
@@ -0,0 +1,46 @@
+const defaultProducts = [
+  {
+    id: "1",
+    name: "Lace Bralette",
+    description: "Beautiful lace bralette",
+    price: 39.99,
+    originalPrice: 59.99,
+    discount: "33% OFF",
+    category: "bra",
+    colors: ["black", "nude"],
+    sizes: ["S", "M", "L"],
+    image: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?w=400",
+    available: true,
+    stock: 50
+  },
+  {
+    id: "2",
+    name: "Silk Panties",
+    description: "Luxurious silk panties",
+    price: 24.99,
+    originalPrice: 34.99,
+    discount: "28% OFF",
+    category: "underwear",
+    colors: ["white", "pink"],
+    sizes: ["XS", "S", "M"],
+    image: "https://images.unsplash.com/photo-1483985988355-763728e1935b?w=400",
+    available: true,
+    stock: 30
+  }
+];
+
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  if (req.method === 'GET') {
+    return res.status(200).json(defaultProducts);
+  }
+  
+  return res.status(405).json({ error: 'Method not allowed' });
+};
