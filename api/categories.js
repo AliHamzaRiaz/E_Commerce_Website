28becce715869641054dd3482f9a90d735589323
AliHamzaRiaz <hamzagujjarriaz3@gmail.com>
2026-06-11 10:03:49 -0700

    Final Vercel API fix

--- api/categories.js
+++ api/categories.js
@@ -7,16 +7,16 @@ const fallbackCategories = [
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  
+
   if (req.method === 'GET') {
     return res.status(200).json(fallbackCategories);
   }
-  
+
   return res.status(405).json({ error: 'Method not allowed' });
 };
