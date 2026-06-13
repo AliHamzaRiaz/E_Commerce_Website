5c931267cc0e9c4037d187a52f165b452f81a253
AliHamzaRiaz <hamzagujjarriaz3@gmail.com>
2026-06-13 03:14:23 -0700

    Fix GitHub Pages deployment: switch to HashRouter and add 404.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import { AuthProvider } from './context/AuthContext'
 
@@ -15,13 +15,13 @@ if (!rootElement) {
 } else {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
   )
 }
