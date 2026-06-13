eaef9ea0a2d7da8cab66f863a7edbf4cc00516a0
AliHamzaRiaz <hamzagujjarriaz3@gmail.com>
2026-06-13 15:18:05 -0700

    Add test route /api/test

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,10 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: "API working" });
+});
+
 const start = async () => {
   try {
     await initProductsDb();
